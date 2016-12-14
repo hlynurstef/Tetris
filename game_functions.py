d7f4d525f317ed84e54158c68f6bb785713c235a
Hlynur Stefánsson <hlynurs15@ru.is>
2016-12-14 03:44:24 +0000

    Add rotate_counterclockwise function

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ from block import Block
 def update_screen(screen, settings):
     """Update everything on screen and then draw the screen."""
     screen.fill(settings.black)
-    
+
     pygame.display.update()
 
 
@@ -15,6 +15,14 @@ def check_events():
         if event.type == pygame.QUIT:
             quit_game()
 
+def rotate_clockwise(shape):
+	outer = []
+	for x in reversed(range(len(shape[0]))):
+		inner = []
+		for y in range(len(shape)):
+			inner.append(shape[y][x])
+		outer.append(inner)
+	return outer
 
 def quit_game():
     """Quits pygame and python."""
