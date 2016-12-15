ed1006de8c9e402a71cc254ed1ea445af6ae7a24
GD <gdg@gdg.is>
2016-12-15 01:36:41 +0000

    changed the layout of the shapes

--- utilities.py
+++ utilities.py
@@ -8,23 +8,32 @@ class Utilities():
 
         self.settings = Settings()
         # Shapes
-        self.shapes = [([[1, 1, 1],      # T Shape
+        self.shapes = [([[0, 0, 0],
+                         [1, 1, 1],      # T Shape
                          [0, 1, 0]], self.settings.T_block),
 
-                       ([[0, 2, 2],      # S Shape
+                       ([[0, 0, 0],
+                         [0, 2, 2],      # S Shape
                          [2, 2, 0]], self.settings.S_block),
 
-                       ([[3, 3, 0],      # Z Shape
+                       ([[0, 0, 0],
+                         [3, 3, 0],      # Z Shape
                          [0, 3, 3]], self.settings.Z_block),
 
-                       ([[4, 4, 4],      # J Shape
+                       ([[0, 0, 0],
+                         [4, 4, 4],      # J Shape
                          [0, 0, 4]], self.settings.J_block),
 
-                       ([[5, 5, 5],      # L Shape
+                       ([[0, 0, 0],
+                         [5, 5, 5],      # L Shape
                          [5, 0, 0]], self.settings.L_block),
 
                                         # I Shape
-                       ([[6, 6, 6, 6]], self.settings.I_block),
+                       ([[0, 0, 0, 0],
+                         [6, 6, 6, 6]
+                         [0, 0, 0, 0]], self.settings.I_block),
 
-                       ([[7, 7],         # O Shape
-                         [7, 7]], self.settings.O_block)]
+                       ([[0, 0, 0, 0],
+                         [0, 7, 7, 0],         # O Shape
+                         [0, 7, 7, 0],
+                         [0, 0, 0, 0]], self.settings.O_block)]
