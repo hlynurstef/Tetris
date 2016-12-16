b1fcd1099fabaf67706f368cc3f15b392df6c817
Hlynur Stefánsson <hlynurs15@ru.is>
2016-12-16 04:16:41 +0000

    Trim music tracks so they loop

--- sounds.py
+++ sounds.py
@@ -1,4 +1,5 @@
 from pygame.mixer import Sound
+from game_settings import Settings
 
 class Sounds():
     """A class to store all sounds in Tetris."""
@@ -10,3 +11,13 @@ class Sounds():
         self.b_type_music = Sound('sounds/music/b_type_music.ogg')
         self.c_type_music = Sound('sounds/music/c_type_music.ogg')
         self.high_score_music = Sound('sounds/music/high_score_music.ogg')
+        self.set_volume()
+
+
+    def set_volume(self):
+        """Set volume of music."""
+        self.title_music.set_volume(.75)
+        self.a_type_music.set_volume(.75)
+        self.b_type_music.set_volume(.75)
+        self.c_type_music.set_volume(.75)
+        self.high_score_music.set_volume(.75)
