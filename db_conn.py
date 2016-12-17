69e151bd14f2e2e4a3621e74089cde10b4eaccda
GD <gdg@gdg.is>
2016-12-17 17:51:41 +0000

    Added a small database to manage the highscores

---
+++ db_conn.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+class DB_conn():
+    """A class that talks to the database, currently SQLite3"""
+
+    def __init__(self):
+        """Open up the connection to the db"""
+        self.conn = sqlite3.connect('highscore.db')
+        self.c = self.conn.cursor()
+        self.create_table()
+
+
+    def create_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS high_score(
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name varchar,
+                        score int NOT NULL)''')
+
+
+    def add_score(self, name, score):
+        self.c.execute(''' INSERT INTO high_score(name, score)
+                        VALUES(?,?)''', (name, str(score)))
+
+    def get_top_ten(self):
+        self.c.execute('SELECT H.name, H.score FROM high_score as H')
+        print(self.c.fetchmany(10))
+
+    def close_connection(self):
+        self.conn.commit()
+        self.conn.close()
